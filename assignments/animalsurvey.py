4395d339545ae5dd3b797ae2527c92f2af939fed
AdeleineIsDead <82011L@mystma.org>
2024-09-25 09:50:45 -0500

    data conversions

--- assignments/animalsurvey.py
+++ assignments/animalsurvey.py
@@ -1,6 +1,6 @@
-user_input1 = input("What is your favorite animal\n >")
-user_input2 = input("why are " + user_input1 + " your favorite animal?\n >")
-user_input3 = input("Where do " + user_input1 + " live? \n>")
-user_input4 = input("What is a fun fact about them\n >")
-print("Your favorite animal is " + user_input1 + " becaus e" + user_input2 + "." + user_input1 + " lives in " + user_input3 + ". A fun fact about " + user_input1 + " is " + user_input4)
+user_input1 = input("What is your favorite animal\n >") #user_input1 is the animal
+user_input2 = input("why are " + user_input1 + " your favorite animal?\n >") #user_iinput2 is the reason
+user_input3 = input("Where do " + user_input1 + " live? \n>") #user_input3 is the location
+user_input4 = input("What is a fun fact about them\n >") #user_input4 is the fact
+print("Your favorite animal is " + user_input1 + " becaus e" + user_input2 + "." + user_input1 + " lives in " + user_input3 + ". A fun fact about " + user_input1 + " is " + user_input4) #the mitochondria is the powerhouse of the cell
 
